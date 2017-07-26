7a4d56ca8309c817a11bfd06d4fed5115fae920b
numb86 <86numb@gmail.com>
2017-07-26 22:11:48 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            'es2015', 'flow', 'react',
+          ],
+          plugins: ['transform-class-properties'],
+        },
+      }],
+    }],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true,
+  },
+};
+
+module.exports = config;

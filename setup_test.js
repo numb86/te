0c6efb023fb20be781cfbbb214af715b7ef1123e
numb86 <86numb@gmail.com>
2018-02-13 11:29:50 +0900

    mount を使えるようにするため、テストの設定ファイルを編集した。

--- setup_test.js
+++ setup_test.js
@@ -2,8 +2,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {JSDOM} from 'jsdom';
 /* eslint-enable import/no-extraneous-dependencies */
 
 Enzyme.configure({adapter: new Adapter()});
 
-global.document = {};
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+
+global.window = jsdom.window;
+global.document = window.document;
